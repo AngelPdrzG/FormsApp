8bac53a0fe81c9dba15a25bd8718ad96c5b30569
Campuzano90 <carlos.javier.giles@hotmail.com>
2023-08-18 14:24:09 -0600

    Clase 240 - Error tipografico en dynPage.component

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './dynamic-page.component.html',
+  styles: [],
+})
+export class DynamicPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get favouriteGamesControl() {
+    return this.myForm.get('favouriteGames') as FormArray;
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, i: number) {
+    return formArray.controls[i].errors && formArray.controls[i].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minLength':
+          return `Minimo ${errors['minLength'].requiredLength} characters`;
+      }
+    }
+
+    return null;
+  }
+
+  onDeleteFavourite(i: number): void {
+    this.favouriteGamesControl.removeAt(i);
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+}
