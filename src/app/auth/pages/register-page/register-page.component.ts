764b8f885388de55f6fad9f2cdf6312d211eddea
Campuzano90 <carlos.javier.giles@hotmail.com>
2023-08-21 10:58:09 -0600

    Clase 250 Validaciones normales y personalizadas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as CustomValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(CustomValidators.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(CustomValidators.emailPattern)],
+    ],
+    username: ['', [Validators.required, CustomValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
