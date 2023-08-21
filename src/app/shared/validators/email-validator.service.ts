b3fb28b77d3d6c3659d7ae95063f13a2d633c5fa
Campuzano90 <carlos.javier.giles@hotmail.com>
2023-08-21 12:36:38 -0600

    clase 254 - Verificacion contraseñas iguales

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -4,11 +4,45 @@ import {
   AsyncValidator,
   ValidationErrors,
 } from '@angular/forms';
-import { Observable, delay, of } from 'rxjs';
+import { Observable, delay } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidatorService implements AsyncValidator {
   validate(
+    control: AbstractControl<any, any>
+  ): Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+
+        if (email === 'fernando@google.com') {
+          subscriber.next({ emailTaken: true });
+        } else {
+          subscriber.next(null);
+        }
+
+        subscriber.complete();
+        return;
+      }
+    ).pipe(delay(3000));
+
+    return httpCallObservable;
+  }
+
+  /* validate(
+    control: AbstractControl<any, any>
+  ): Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    return this.http.get<any>(`https://localhost:3000/users?q=${email}`).pipe(
+    map((resp) => {
+      return resp.length === 0 ? null : { emailTaken: true };
+    })
+  ); */
+
+  /* validate(
     control: AbstractControl<any, any>
   ): Observable<ValidationErrors | null> {
     const email = control.value;
@@ -17,5 +51,5 @@ export class EmailValidatorService implements AsyncValidator {
     return of({
       emailTaken: true,
     }).pipe(delay(2000));
-  }
+  } */
 }
